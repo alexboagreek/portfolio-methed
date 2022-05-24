f2df67339423831dbaf91daed3a6507bd17a17de
Alex <olivesargreek@gmail.com>
2022-05-24 10:26:33 +0300

    feat: modal styles, close modal styles, magic with jQuerry

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.portfolio__arrow_right',
+        prevEl: '.portfolio__arrow_left',
+        disabledClass: 'portfolio__arrow_disable',
+    },
+    breakpoints: {
+        480: {
+           grid: {
+               fill: 'row',
+               rows: 2
+           } 
+        },
+        768: {
+            slidesPerView: 2,
+            grid: {
+                fill: 'row',
+                rows: 2
+            } 
+         }
+    }
+});
+
+$('.header__contacts-burger').click(function () {
+    $('.header__contacts').slideToggle();
+});
+
+$('.present__order-btn').click(() => {
+    $('.page__overlay_modal').fadeIn(400).css('display', 'flex');
+});
+$('.modal__close').click(() => {
+    $('.page__overlay_modal').fadeOut(400);
+})
